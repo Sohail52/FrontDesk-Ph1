822b07b90726bfe0333aa4c83cec6c97a227d3e7
Syed Sohail <ssohailm07@gmail.com>
2025-10-19 19:16:43 +0530

    Made updates to frontend

--- frontend/backend/models/helpRequest.js
+++
@@ -1,9 +0,0 @@
-export class HelpRequest {
-  constructor(question, callerId) {
-    this.id = Date.now().toString()        // unique id
-    this.question = question
-    this.callerId = callerId
-    this.status = "pending"
-    this.timestamp = new Date().toISOString()
-  }
-}
